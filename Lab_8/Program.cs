1b666b75bbd08c06b9a3218a8db1feda9b0c8ec3
vermei2@outlook.com <vermei2@outlook.com>
2022-01-18 22:27:04 +0300

    Добавьте файлы проекта.

---
+++ Lab_8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //Вывести содержимое папки на консоль
+            string path = @"C:\123";
+                Vivod(path);
+        Console.ReadKey();
+        }
+        static void Vivod(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+            if (dirs.Count() > 0 || files.Count() > 0)
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                    Console.WriteLine();
+                }
+            foreach (string s in dirs)
+            {
+                Console.WriteLine(s);
+                Console.WriteLine();
+                Vivod(s);
+            }   
+            
+        }
+    }
+}
---
+++ laB_8_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace laB_8_2
+{
+    class Program
+    {//Создать файл, записать 10 чисел, открыть файл, рассчитать сумму чисел в нем, вывести ответ
+        static void Main(string[] args)
+        {
+            string path = "Logs/Log.txt";
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                int[] array = new int[10];
+                Random random = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = random.Next(0, 1000);
+                    sw.WriteLine("{0}", array[i]);
+                }
+            }
+            using (StreamReader sw = new StreamReader(path, true))
+            /* {
+                 sw.ReadLine();
+                 { Console.WriteLine(sw.ReadToEnd()); }
+                 Console.ReadKey();
+             }*/
+            {
+                int[] intChislo = File.ReadAllLines(path).Select(int.Parse).ToArray();
+                int sum = intChislo.Sum();
+                Console.WriteLine("Сумма чисел равна {0}", sum);
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
